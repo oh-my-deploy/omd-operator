39f86fcf897d0392f3ba5857dec20b07c91608df
tae2089 <tae2089002@gmail.com>
2024-05-13 07:44:31 +0900

    :art: Update spec

--- api/v1alpha1/program_types.go
+++ api/v1alpha1/program_types.go
@@ -17,19 +17,98 @@ limitations under the License.
 package v1alpha1
 
 import (
+	v2 "k8s.io/api/autoscaling/v2"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
+type AppSpec struct {
+	Image         string            `json:"image"`
+	ContainerPort int32             `json:"containerPort"`
+	Replicas      *int32            `json:"replicas,omitempty"`
+	AppType       string            `json:"appType,omitempty"` // back, front-spa, front-srr
+	Annotations   map[string]string `json:"annotations,omitempty"`
+	Probe         ProbeSpec         `json:"probe,omitempty"`
+}
+
+type ProbeSpec struct {
+	Startup   *v1.Probe `json:"startup,omitempty"`
+	Liveness  *v1.Probe `json:"liveness,omitempty"`
+	Readiness *v1.Probe `json:"readiness,omitempty"`
+}
+
+type PodDisruptionBudgetSpec struct {
+	// +optional
+	Enabled *bool `json:"enabled,omitempty"`
+	// +optional
+	MinAvailable *int32 `json:"minAvailable,omitempty"`
+	// +optional
+	MaxUnavailable *int32 `json:"maxUnavailable,omitempty"`
+}
+
+type ServiceSpec struct {
+	Enabled     bool              `json:"enabled"`
+	Annotations map[string]string `json:"annotations,omitempty"`
+}
+
+type IngressSpec struct {
+	Enabled     bool              `json:"enabled"`
+	Annotations map[string]string `json:"annotations,omitempty"`
+	Rules       IngressRulesSpec  `json:"rules"`
+}
+
+type IngressRulesSpec struct {
+	Host string `json:"host,omitempty"`
+	// +listType=atomic
+	Paths []IngressPath `json:"paths"`
+}
+
+type IngressPath struct {
+	// +optional
+	Path string `json:"path,omitempty"`
+	// +optional
+	ServiceName string `json:"serviceName,omitempty"`
+	// +optional
+	Port *int32 `json:"port,omitempty"`
+}
+
+type ServiceAccountSpec struct {
+	Create *bool `json:"create,omitempty"`
+	// +optional
+	Annotations map[string]string `json:"annotations,omitempty"`
+	// +optional
+	AutomountServiceAccountToken *bool  `json:"automountServiceAccountToken,omitempty"`
+	ServiceAccountName           string `json:"serviceAccountName,omitempty"`
+}
+
+type HorizontalPodAutoScalerSpec struct {
+	Enabled     *bool             `json:"enabled,omitempty"`
+	Annotations map[string]string `json:"annotations,omitempty"`
+	MinReplicas *int32            `json:"minReplicas,omitempty"`
+	MaxReplicas *int32            `json:"maxReplicas,omitempty"`
+	Metrics     []v2.MetricSpec   `json:"metrics,omitempty"`
+}
+
+type SchedulerSpec struct {
+	NodeSelector            map[string]string            `json:"nodeSelector,omitempty"`
+	PodDisruptionBudget     *PodDisruptionBudgetSpec     `json:"pdb,omitempty"`
+	HorizontalPodAutoScaler *HorizontalPodAutoScalerSpec `json:"hpa,omitempty"`
+	Affinity                *v1.Affinity                 `json:"affinity,omitempty"`
+}
+
 // ProgramSpec defines the desired state of Program
 type ProgramSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
-
 	// Foo is an example field of Program. Edit program_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	Foo            string              `json:"foo,omitempty"`
+	Service        *ServiceSpec        `json:"service,omitempty"`
+	ServiceAccount *ServiceAccountSpec `json:"serviceAccount,omitempty"`
+	Ingress        *IngressSpec        `json:"ingress,omitempty"`
+	Scheduler      *SchedulerSpec      `json:"scheduler,omitempty"`
 }
 
 // ProgramStatus defines the observed state of Program
